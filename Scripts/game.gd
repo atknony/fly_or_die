b15e6086791feed2bb58f783b6e3d51287dfeb0b
ivorionn <yagiz.tuten@gmail.com>
2025-06-20 20:08:38 +0300

    SA

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var player: CharacterBody2D = $Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_player_entered(body: Node2D) -> void:
+	player.transform.x *= -1
+	player.velocity.x *= -1
