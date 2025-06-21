6223af11c75f35fd49e695fa9ba8a982b068fdf5
ivorionn <yagiz.tuten@gmail.com>
2025-06-21 10:57:26 +0300

    Spike Screen Exited Signal

--- Scripts/spike.gd
+++ Scripts/spike.gd
@@ -8,10 +8,10 @@ const OFF_SCREEN: float = 1000.0
 func _process(delta: float) -> void:
 	SPEED += ACCELERATION * delta 
 	position.y += SPEED * delta 
-	if position.y > get_viewport_rect().position.y + OFF_SCREEN:
-		queue_free()
-	
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
+
+
+func _on_2d_screen_exited() -> void:
+	queue_free()
